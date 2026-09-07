9893ebe6442a4d497962e47c60cdd55c67bf8e86
Voltaris Mobility <voltaris.rw@gmail.com>
2026-09-07 14:02:15 +0200

    fix form input text color

--- src/features/home/ContactHome.tsx
+++ src/features/home/ContactHome.tsx
@@ -130,7 +130,7 @@ export function ContactHome() {
                         {...props}
                         {...register('full_name')}
                         autoComplete="name"
-                        className={`${inputClass} !bg-white !text-[#171411] placeholder:!text-[#8a837a]`}
+                        className={`${inputClass} !bg-white !text-black !caret-black placeholder:!text-[#8a837a]`}
                       />
                     )}
                   </Field>
@@ -143,7 +143,7 @@ export function ContactHome() {
                           {...register('email')}
                           type="email"
                           autoComplete="email"
-                          className={`${inputClass} !bg-white !text-[#171411] placeholder:!text-[#8a837a]`}
+                          className={`${inputClass} !bg-white !text-black !caret-black placeholder:!text-[#8a837a]`}
                         />
                       )}
                     </Field>
@@ -160,7 +160,7 @@ export function ContactHome() {
                           {...register('phone')}
                           type="tel"
                           autoComplete="tel"
-                          className={`${inputClass} !bg-white !text-[#171411] placeholder:!text-[#8a837a]`}
+                          className={`${inputClass} !bg-white !text-black !caret-black placeholder:!text-[#8a837a]`}
                         />
                       )}
                     </Field>
@@ -190,7 +190,7 @@ export function ContactHome() {
                         {...register('message')}
                         rows={6}
                         placeholder="For example, I'm looking for an affordable hybrid for daily driving in Kigali..."
-                        className={`${inputClass} !bg-white !text-[#171411] placeholder:!text-[#8a837a]`}
+                        className={`${inputClass} !bg-white !text-black !caret-black placeholder:!text-[#8a837a]`}
                       />
                     )}
                   </Field>
